24553190fd13a21f4e8026343472515a46ca180e
Grant Fellows <grant.fellows@mobilefun.co.uk>
2017-06-02 14:02:30 +0100

    Shows difference between single and double quote in PHP

--- 003-classes-this-attributes.php
+++ 003-classes-this-attributes.php
@@ -24,3 +24,7 @@ $myClass = new SimpleClass();
 $myClass->setVar("Apple");
 $myClass->setVar2("Banana");
 echo $myClass->getVar() . PHP_EOL;
+
+$fruit = $myClass->getVar();
+echo "I like $fruit's" . PHP_EOL;
+echo 'I like $fruit' . PHP_EOL;
